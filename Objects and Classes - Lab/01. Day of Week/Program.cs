f235fe160bfe5fa81f2053d9596560cab3c09bd4
theoners <theonebgrs@gmail.com>
2018-11-14 21:38:10 +0100

    Objects and Classes - Lab

---
+++ Objects and Classes - Lab/01. Day of Week/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace _01._Day_of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                string input = Console.ReadLine();
+                DateTime date = DateTime.ParseExact(input, "d-M-yyyy", CultureInfo.InvariantCulture);
+                Console.WriteLine(date.DayOfWeek);
+        }
+    }
+}
---
+++ Objects and Classes - Lab/02. Randomize Words/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02._Randomize_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            Random randomNumber = new Random();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                int randomIndex = randomNumber.Next(0, words.Length - 1);
+                string currentWord = words[i];
+                words[i] = words[randomIndex];
+                words[randomIndex] = currentWord;
+            }
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
